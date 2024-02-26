ee7b635d617e2ea71d87a7d4be30ac17912f59bf
Robin Warner <rowaniar@netscape.net>
2024-02-26 17:35:33 +0000

    Add bug sitting

--- src/components/Bug.js
+++ src/components/Bug.js
@@ -20,6 +20,7 @@ export default function Bug({
     const bugNum = useRef();
     const [bugActive, setBugActive] = useState(false);
     const numBugs = useRef();
+    const landingSites = useRef();
     const app = useApp();
 
     const imageNameFromFile = (filename) => {
@@ -43,9 +44,9 @@ export default function Bug({
         return texture;
     }
 
-    // Set-up the bug data initially
+    // Set-up the bug data and stage landing sites initially
     useEffect( () => {
-        if (initial && globalImageData.length) {
+        const setupBugData = () => {
             // Search for the bug information in the graphic data
             let gotBugs = false;
             let bugIndex = 0;
@@ -70,9 +71,37 @@ export default function Bug({
                 numBugs.current = bugData.current.length;
                 setBugStart(true);
             }
+        }
+
+        const setupLandingSites = () => {
+            // Search the image property data
+            let found = false;
+            for (let item of globalImageData) {
+                if (item.type === "stage") {
+                    landingSites.current = item.landingSites;
+                    found = true;
+                }
+            }
+            if (!found) {
+                console.log("prop-files data does not contain landing sites");
+            }
+            else { 
+                // Scale the dimensions and positions
+                for (let site of landingSites.current) {
+                    site.x = site.x * stageScale;
+                    site.y = site.y * stageScale;
+                    site.width = site.width * stageScale;
+                    site.height = site.height * stageScale;
+                }
+            }
+        }
+
+        if (initial && globalImageData.length) {
+            setupBugData();
+            setupLandingSites();
             setInitial(false);
         }
-    }, [initial, globalImageData, setBugStart, app])
+    }, [initial, stageScale, globalImageData, setBugStart, app])
 
     // Set-up a new bug for activity
     useEffect(() => {
@@ -119,92 +148,139 @@ export default function Bug({
 
     // Handle the active state of the current bug
     useEffect(() => {
+        const doChangesToMotion = () => {
+            if (counter - activeBugData.current.lastMotionChangeStep > GLOBALS.motionChangeSteps && Math.random() > 0.1) {
+                let deltax = GLOBALS.maxAccelerationChange * 0.2 + Math.random() * (GLOBALS.maxAccelerationChange * 0.8);
+                if (activeBugData.current.x > stageWidth || activeBugData.current.x < 0) {
+                    deltax = 0;
+                }
+                else if (activeBugData.current.ax < 0 && Math.random() > 0.97) {
+                    deltax = -deltax;
+                }
+                else if (Math.random() < 0.5) {
+                    deltax = -deltax;
+                }
+
+                if (deltax + activeBugData.current.ax < GLOBALS.maxAcceleration && 
+                    deltax + activeBugData.current.ax > -GLOBALS.maxAcceleration) {
+                    activeBugData.current.ax += deltax;
+                    activeBugData.current.lastMotionChangeStep = counter;
+                }
+
+                let deltay = GLOBALS.maxAccelerationChange * 0.2 + Math.random() * (GLOBALS.maxAccelerationChange * 0.8);
+                if (activeBugData.current.y < 0 || activeBugData.current.y > stageHeight) {
+                    deltay = 0;
+                }
+                else if (activeBugData.current.y > stageHeight * 0.68 && Math.random() < 0.8) {
+                    deltay = -deltay;
+                }
+                else if (Math.random() > 0.8) {
+                    deltay = -deltay;
+                }
+
+                if (deltay + activeBugData.current.ay < GLOBALS.maxAcceleration && 
+                    deltay + activeBugData.current.ay > -GLOBALS.maxAcceleration) {
+                    activeBugData.current.ay += deltay;
+                    activeBugData.current.lastMotionChangeStep = counter;
+                }
+            }
+        }
+
+        const performMotion = () => {
+            // Adjust velocity
+            activeBugData.current.vx += activeBugData.current.ax;
+            if (activeBugData.current.vx > GLOBALS.maxSpeed) {
+                activeBugData.current.vx = GLOBALS.maxSpeed;
+                activeBugData.current.ax = 0;
+            }
+            else if (activeBugData.current.vx < -GLOBALS.maxSpeed) {
+                activeBugData.current.vx = -GLOBALS.maxSpeed;
+                activeBugData.current.ax = 0;
+            }
+
+            activeBugData.current.vy += activeBugData.current.ay;
+            if (activeBugData.current.vy > GLOBALS.maxSpeed) {
+                activeBugData.current.vy = GLOBALS.maxSpeed;
+                activeBugData.current.ay = 0;
+            }
+            else if (activeBugData.current.vy < -GLOBALS.maxSpeed) {
+                activeBugData.current.vy = -GLOBALS.maxSpeed;
+                activeBugData.current.ay = 0;
+            }
+
+            // Adjust position
+            activeBugData.current.x = activeBugData.current.x + activeBugData.current.vx;
+            activeBugData.current.y = activeBugData.current.y + activeBugData.current.vy;
+            if (activeBugData.current.x < 0) {
+                activeBugData.current.vx = Math.abs(activeBugData.current.vx);
+                activeBugData.current.ax = Math.abs(activeBugData.current.ax);
+            }
+            else if (activeBugData.current.x >= stageWidth) {
+                activeBugData.current.vx = -Math.abs(activeBugData.current.vx);
+                activeBugData.current.ax = -Math.abs(activeBugData.current.ax);
+            }
+            if (activeBugData.current.y < 0) {
+                activeBugData.current.vy = Math.abs(activeBugData.current.vy);
+                activeBugData.current.ay = Math.abs(activeBugData.current.ay);
+            }
+            else if (activeBugData.current.y > stageWidth) {
+                activeBugData.current.vy = -Math.abs(activeBugData.current.vy);
+                activeBugData.current.ay = -Math.abs(activeBugData.current.ay);
+            }
+
+            if ((activeBugData.current.vx < 0 && activeBugData.current.defaultOrientation === "rightward") ||
+                (activeBugData.current.vx > 0 && activeBugData.current.defaultOrientation === "leftward")) {
+                activeBugData.current.scaleX = -1;
+                activeBugData.current.scaleY = 1;
+            }
+            else {
+                activeBugData.current.scaleX = 1;
+                activeBugData.current.scaleY = 1;
+            }
+        }
+
+        const checkForLanding = () => {
+            let nearSite = false;
+            let site;
+            for (site of landingSites.current) {
+                if ((site.x - 5 < activeBugData.current.x && site.x + site.width + 5 > activeBugData.current.x) &&
+                    (site.y - 5 < activeBugData.current.y && site.y + site.height + 5 > activeBugData.current.y)) {
+                    nearSite = true;
+                    break;
+                }
+            }
+
+            if (nearSite && Math.random() > 0.995) {
+                // Do the landing
+                activeBugData.current.status = "sitting";
+                activeBugData.current.sitStart = counter;
+                activeBugData.current.image = bugData.current[bugNum.current].sitting.image;
+                // Set the position
+                activeBugData.current.x = site.x + site.width / 2;
+                activeBugData.current.y = site.y + site.height / 2;
+            }
+        }
+
         const moveBug = () => {
             if (bugActive) {
-                // Check for changes to motion
-                if (counter - activeBugData.current.lastMotionChangeStep > GLOBALS.motionChangeSteps && Math.random() > 0.1) {
-                    console.log("Got to change acceleration")
-                    let deltax = GLOBALS.maxAccelerationChange * 0.2 + Math.random() * (GLOBALS.maxAccelerationChange * 0.8);
-                    if (activeBugData.current.x > stageWidth || activeBugData.current.x < 0) {
-                        deltax = 0;
-                    }
-                    else if (Math.random() > 0.5) {
-                        deltax = -deltax;
-                    }
-                    if (deltax + activeBugData.current.ax < GLOBALS.maxAcceleration && 
-                        deltax + activeBugData.current.ax > -GLOBALS.maxAcceleration) {
-                        activeBugData.current.ax += deltax;
-                        activeBugData.current.lastMotionChangeStep = counter;
-                        console.log("Change acceleration", activeBugData.current.ax, deltax);
-                    }
+                // Check if bug sitting
+                if (activeBugData.current.status === "sitting" && 
+                    counter - activeBugData.current.sitStart > GLOBALS.sitTime && 
+                    Math.random() > 0.3) {
+                    // Put back in flight
+                    activeBugData.current.status = "flying";
+                    activeBugData.current.image = bugData.current[bugNum.current].flying.image;
+                    setActiveBugDataState(activeBugData.current);
+                }
+                else if (activeBugData.current.status === "flying") {
+                    // Check for changes to motion
+                    doChangesToMotion();
+                    performMotion();
+                    checkForLanding();
 
-                    let deltay = GLOBALS.maxAccelerationChange * 0.2 + Math.random() * (GLOBALS.maxAccelerationChange * 0.8);
-                    if (activeBugData.current.y < 0 || activeBugData.current.y > stageHeight) {
-                        deltay = 0;
-                    }
-                    else if (Math.random() > 0.5) {
-                        deltay = -deltay;
-                    }
-
-                    if (deltay + activeBugData.current.ay < GLOBALS.maxAcceleration && 
-                        deltay + activeBugData.current.ay > -GLOBALS.maxAcceleration) {
-                        activeBugData.current.ay += deltay;
-                        activeBugData.current.lastMotionChangeStep = counter;
-                    }
+                    // Update the state
+                    setActiveBugDataState(activeBugData.current);
                 }
-                // Adjust velocity
-                activeBugData.current.vx += activeBugData.current.ax;
-                if (activeBugData.current.vx > GLOBALS.maxSpeed) {
-                    activeBugData.current.vx = GLOBALS.maxSpeed;
-                    activeBugData.current.ax = 0;
-                }
-                else if (activeBugData.current.vx < -GLOBALS.maxSpeed) {
-                    activeBugData.current.vx = -GLOBALS.maxSpeed;
-                    activeBugData.current.ax = 0;
-                }
-
-                activeBugData.current.vy += activeBugData.current.ay;
-                if (activeBugData.current.vy > GLOBALS.maxSpeed) {
-                    activeBugData.current.vy = GLOBALS.maxSpeed;
-                    activeBugData.current.ay = 0;
-                }
-                else if (activeBugData.current.vy < -GLOBALS.maxSpeed) {
-                    activeBugData.current.vy = -GLOBALS.maxSpeed;
-                    activeBugData.current.ay = 0;
-                }
-
-                // Adjust position
-                activeBugData.current.x = activeBugData.current.x + activeBugData.current.vx;
-                activeBugData.current.y = activeBugData.current.y + activeBugData.current.vy;
-                if (activeBugData.current.x < 0) {
-                    activeBugData.current.vx = Math.abs(activeBugData.current.vx);
-                    activeBugData.current.ax = Math.abs(activeBugData.current.ax);
-                }
-                else if (activeBugData.current.x >= stageWidth) {
-                    activeBugData.current.vx = -Math.abs(activeBugData.current.vx);
-                    activeBugData.current.ax = -Math.abs(activeBugData.current.ax);
-                }
-                if (activeBugData.current.y < 0) {
-                    activeBugData.current.vy = Math.abs(activeBugData.current.vy);
-                    activeBugData.current.ay = Math.abs(activeBugData.current.ay);
-                }
-                else if (activeBugData.current.y > stageWidth) {
-                    activeBugData.current.vy = -Math.abs(activeBugData.current.vy);
-                    activeBugData.current.ay = -Math.abs(activeBugData.current.ay);
-                }
-
-                if ((activeBugData.current.vx < 0 && activeBugData.current.defaultOrientation === "rightward") ||
-                    (activeBugData.current.vx > 0 && activeBugData.current.defaultOrientation === "leftward")) {
-                    activeBugData.current.scaleX = -1;
-                    activeBugData.current.scaleY = 1;
-                }
-                else {
-                    activeBugData.current.scaleX = 1;
-                    activeBugData.current.scaleY = 1;
-                }
-
-                // Update the state
-                setActiveBugDataState(activeBugData.current);
                 let c = counter + 1; 
                 setCounter(c);
                 if (c > activeBugData.current.numBugSteps) {
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,12 @@
 const GLOBALS = {
     graphicDataFile: "prop-files.json",
     graphicDataPath: "/static/graphics/",
-    maxSpeed: 1.0,
+    maxSpeed: 1.5,
     maxAcceleration: 0.2,
     maxAccelerationChange: 0.05,
     maxBugSteps: 10000,
-    motionChangeSteps: 50
+    motionChangeSteps: 50,
+    sitTime: 100
 }
 
 export default GLOBALS;
