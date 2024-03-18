4d9d92e2ed3d9c831b06945126f6c17c967f8f77
Robin Warner <rowaniar@netscape.net>
2024-03-18 21:26:12 +0000

    Try eventmode static

--- src/components/ScoreChart.js
+++ src/components/ScoreChart.js
@@ -139,7 +139,7 @@ export default function ScoreChart({
     }, [gameEnd, clearGameEnd])
 
     return (
-        <Stage width={scoreChartWidth} height={scoreChartHeight} eventmode={"auto"} touchaction={"auto"}>
+        <Stage width={scoreChartWidth} height={scoreChartHeight} eventmode={"static"} touchaction={"auto"}>
             {!initial && 
             <>
             <Sprite 
